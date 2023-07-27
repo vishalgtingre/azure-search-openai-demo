b6871dd3f4a85c445a89009c15efca3488c8abca
Vishal Tingre <vishal.tingre@elastic.run>
2023-07-27 22:21:05 +0000

    updates

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What is Modern Portfolio Theory?",
+        value: "What is Modern Portfolio Theory?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What is a Quadratic Assignment Problem ?", value: "What is a Quadratic Assignment Problem ?" },
+    { text: "What are Combinatorial optimization Problems", value: "What is the use of Quantum Computing?" }
 ];
 
 interface Props {
