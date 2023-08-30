71ec26c878cc5c6f410322df4a204ddf90fc74cf
Vishal Tingre <vishal.tingre@elastic.run>
2023-08-30 10:50:20 +0000

    update error handling

--- app/backend/externaldata/chat_code_utils.py
+++ app/backend/externaldata/chat_code_utils.py
@@ -31,38 +31,48 @@ def extract_code_from_chat(message):
 
     return code_blocks
 
+import re
+
 def extract_code_from_message(message):
-    # Extract content between triple backticks
-    orig_code_match = re.search(r'```(.*?)```', message, re.DOTALL)
-    if orig_code_match:
-        code_match = orig_code_match.group(1)
+    try:
+        orig_code_match = re.search(r'```(.*?)```', message, re.DOTALL)
+        if orig_code_match:
+            code_match = orig_code_match.group(1)
 
-        # Remove the word "python" if it exists in the code
-        code_without_python = re.sub(r'\bpython\b', '', code_match, flags=re.IGNORECASE)
+            code_without_python = re.sub(r'\bpython\b', '', code_match, flags=re.IGNORECASE)
 
-        # Define a function to handle print replacements
-        def handle_print(match):
-            print_contents = match.group(1)
-            print_contents = print_contents.replace("\\n", "\n")
-            return f'print({print_contents})'
+            def handle_print(match):
+                print_contents = match.group(1)
+                print_contents = print_contents.replace("\\n", "\n")
+                return f'print({print_contents})'
 
-        # Replace \n outside of print statements
-        code_without_newlines = re.sub(r'(?<!print\()(.*?)\\n(.*?)(?!\))', r'\1\n\2', code_without_python)
+            code_without_newlines = re.sub(r'(?<!print\()(.*?)\\n(.*?)(?!\))', r'\1\n\2', code_without_python)
+            final_code = re.sub(r'print\((.*?)\\n(.*?)\)', handle_print, code_without_newlines)
 
-        # Replace \n inside print statements
-        final_code = re.sub(r'print\((.*?)\\n(.*?)\)', handle_print, code_without_newlines)
+            with open('modified_code.py', 'w') as output_file:
+                output_file.write(final_code)
 
-        return final_code.strip()
+            return final_code.strip()
+        
+        return None
+    except Exception as e:
+        return f"An error occurred in 'extract_code_from_message': {str(e)}"
     
-    return None
-
-
 def appendimportsandprints(code_from_msg):
-    with open('importlib.txt', 'r') as file:
-        importfile_content = file.read()
+    try:
+        with open('importlib.txt', 'r') as import_file:
+            importfile_content = import_file.read()
 
-    with open('printoutput.txt','r') as file:
-        printfile_content = file.read()
-    
-    codetoexecute = importfile_content + code_from_msg + printfile_content
-    return codetoexecute
+        with open('printoutput.txt', 'r') as print_file:
+            printfile_content = print_file.read()
+        
+        codetoexecute = importfile_content + code_from_msg + printfile_content
+        
+        with open('codetoexecute.py', 'w') as output_file:
+            output_file.write(codetoexecute)
+        
+        return codetoexecute
+    except FileNotFoundError as e:
+        return f"Error in 'appendimportsandprints': {e.filename} not found."
+    except Exception as e:
+        return f"An error occurred in 'appendimportsandprints': {str(e)}"
