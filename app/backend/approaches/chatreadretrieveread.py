b831e0e41093259017f4f03fba801454d2885988
vishalgtingre <vishal_tingre@hotmail.com>
2023-09-26 19:28:57 +0000

    updates latest

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -2,6 +2,13 @@ import json
 from typing import Any, AsyncGenerator
 
 import openai
+import pandas as pd
+import json
+import math
+
+import pytz
+from datetime import datetime
+
 from azure.search.documents.aio import SearchClient
 from azure.search.documents.models import QueryType
 
@@ -113,52 +120,47 @@ If you cannot generate a search query, return just the number 0.
                 },
             },
             {
-            "name": "get_current_time",
-            "description": "Get the current time in a given location",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "location": {
-                        "type": "string",
-                        "description": "The location name. The pytz is used to get the timezone for that location. Location names should be in a format like America/New_York, Asia/Bangkok, Europe/London",
-                    }
+                "name": "get_current_time",
+                "description": "Get the current time in a given location",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "location": {
+                            "type": "string",
+                            "description": "The location name. The pytz is used to get the timezone for that location. Location names should be in a format like America/New_York, Asia/Bangkok, Europe/London",
+                        }
+                    },
+                    "required": ["location"],
                 },
-                "required": ["location"],
             },
-        },
-        {
-            "name": "get_stock_market_data",
-            "description": "Get the stock market data for a given index",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "index": {
-                        "type": "string",
-                        "enum": ["S&P 500", "NASDAQ Composite", "Dow Jones Industrial Average", "Financial Times Stock Exchange 100 Index"]},
-                },
-                "required": ["index"],
-            },    
-        },
-        {
-            "name": "calculator",
-            "description": "A simple calculator used to perform basic arithmetic operations",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "num1": {"type": "number"},
-                    "num2": {"type": "number"},
-                    "operator": {"type": "string", "enum": ["+", "-", "*", "/", "**", "sqrt"]},
-                },
-                "required": ["num1", "num2", "operator"],
+            {
+                "name": "get_stock_market_data",
+                "description": "Get the stock market data for a given index",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "index": {
+                            "type": "string",
+                            "enum": ["S&P 500", "NASDAQ Composite", "Dow Jones Industrial Average", "Financial Times Stock Exchange 100 Index"]},
+                    },
+                    "required": ["index"],
+                },    
             },
-        }
-    ]
+            {
+                "name": "calculator",
+                "description": "A simple calculator used to perform basic arithmetic operations",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "num1": {"type": "number"},
+                        "num2": {"type": "number"},
+                        "operator": {"type": "string", "enum": ["+", "-", "*", "/", "**", "sqrt"]},
+                    },
+                    "required": ["num1", "num2", "operator"],
+                },
+            }
+        ]
 
-        available_functions = {
-            "get_current_time": get_current_time,
-            "get_stock_market_data": get_stock_market_data,
-            "calculator": calculator,
-        } 
 
         #investment_keywords = [
         #'Invest money', 'maximise return', 'minimise risk', 'create portfolio',
@@ -358,61 +360,55 @@ If you cannot generate a search query, return just the number 0.
                 return query_text
         return user_query
 
-import pytz
-from datetime import datetime
-def get_current_time(location):
-    try:
-        # Get the timezone for the city
-        timezone = pytz.timezone(location)
 
-        # Get the current time in the timezone
-        now = datetime.now(timezone)
-        current_time = now.strftime("%I:%M:%S %p")
+    def get_current_time(location):
+        try:
+            # Get the timezone for the city
+            timezone = pytz.timezone(location)
 
-        return current_time
-    except:
-        return "Sorry, I couldn't find the timezone for that location."
+            # Get the current time in the timezone
+            now = datetime.now(timezone)
+            current_time = now.strftime("%I:%M:%S %p")
 
-import math
+            return current_time
+        except:
+            return "Sorry, I couldn't find the timezone for that location."
 
-def calculator(num1, num2, operator):
-    if operator == '+':
-        return str(num1 + num2)
-    elif operator == '-':
-        return str(num1 - num2)
-    elif operator == '*':
-        return str(num1 * num2)
-    elif operator == '/':
-        return str(num1 / num2)
-    elif operator == '**':
-        return str(num1 ** num2)
-    elif operator == 'sqrt':
-        return str(math.sqrt(num1))
-    else:
-        return "Invalid operator"
+    def calculator(num1, num2, operator):
+        if operator == '+':
+            return str(num1 + num2)
+        elif operator == '-':
+            return str(num1 - num2)
+        elif operator == '*':
+            return str(num1 * num2)
+        elif operator == '/':
+            return str(num1 / num2)
+        elif operator == '**':
+            return str(num1 ** num2)
+        elif operator == 'sqrt':
+            return str(math.sqrt(num1))
+        else:
+            return "Invalid operator"
 
-import pandas as pd
-import json
+    def get_stock_market_data(index):
+        available_indices = ["S&P 500", "NASDAQ Composite", "Dow Jones Industrial Average", "Financial Times Stock Exchange 100 Index"]
 
-def get_stock_market_data(index):
-    available_indices = ["S&P 500", "NASDAQ Composite", "Dow Jones Industrial Average", "Financial Times Stock Exchange 100 Index"]
+        if index not in available_indices:
+            return "Invalid index. Please choose from 'S&P 500', 'NASDAQ Composite', 'Dow Jones Industrial Average', 'Financial Times Stock Exchange 100 Index'."
 
-    if index not in available_indices:
-        return "Invalid index. Please choose from 'S&P 500', 'NASDAQ Composite', 'Dow Jones Industrial Average', 'Financial Times Stock Exchange 100 Index'."
+        # Read the CSV file
+        data = pd.read_csv('stock_data.csv')
 
-    # Read the CSV file
-    data = pd.read_csv('stock_data.csv')
+        # Filter data for the given index
+        data_filtered = data[data['Index'] == index]
 
-    # Filter data for the given index
-    data_filtered = data[data['Index'] == index]
+        # Remove 'Index' column
+        data_filtered = data_filtered.drop(columns=['Index'])
 
-    # Remove 'Index' column
-    data_filtered = data_filtered.drop(columns=['Index'])
+        # Convert the DataFrame into a dictionary
+        hist_dict = data_filtered.to_dict()
 
-    # Convert the DataFrame into a dictionary
-    hist_dict = data_filtered.to_dict()
+        for key, value_dict in hist_dict.items():
+            hist_dict[key] = {k: v for k, v in value_dict.items()}
 
-    for key, value_dict in hist_dict.items():
-        hist_dict[key] = {k: v for k, v in value_dict.items()}
-
-    return json.dumps(hist_dict)
+        return json.dumps(hist_dict)
